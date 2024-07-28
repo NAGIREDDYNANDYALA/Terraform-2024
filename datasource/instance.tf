de134c31aefd9e8e04d5f5313c54a320fa532796
NAGIREDDYNANDYALA <118894739+NAGIREDDYNANDYALA@users.noreply.github.com>
2024-07-28 12:14:01 +0530

    Update instance.tf

--- datasource/instance.tf
+++ datasource/instance.tf
@@ -1,30 +1,13 @@
-data "aws_availability_zones" "available" {
-  #above line will fetch all the availability zones which are available in AWS
-}
-
-data "aws_ami" "latest-linux" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-2.0.????????.?-x86_64-gp2"]
-
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
 resource "aws_instance" "datasource" {
-  ami               = data.aws_ami.latest-linux.id
-  instance_type     = "t2.micro"
-  availability_zone = data.aws_availability_zones.available.names[1]
+  ami = "ami-0b0ea68c435eb488d"
+  instance_type = "t2.micro"
+
   tags = {
     Name = "datainstance"
   }
 }
 
-/*data "aws_instance" "datasource" {
+data "aws_instance" "datasource" {
 
   filter {
     name   = "tag:Name"
@@ -34,4 +17,4 @@ resource "aws_instance" "datasource" {
 }
 output "data_from_instance" {
   value = data.aws_instance.datasource.public_ip
-}*/
+}
