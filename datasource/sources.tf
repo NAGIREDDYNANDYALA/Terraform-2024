0b703cd61309ae83223c3557d53c679f5c8bb707
NAGIREDDYNANDYALA <118894739+NAGIREDDYNANDYALA@users.noreply.github.com>
2024-07-28 21:52:49 +0530

    Update sources.tf

--- datasource/sources.tf
+++ datasource/sources.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "datasource" {
   }
 }
 
-/*data "aws_instance" "datasource" {
+data "aws_instance" "datasource" {
 
   filter {
     name   = "tag:Name"
@@ -34,4 +34,26 @@ resource "aws_instance" "datasource" {
 }
 output "data_from_instance" {
   value = data.aws_instance.datasource.public_ip
-}*/
+}
+
+# to create Security Group dynamically with help of the data source
+data "aws_ip_ranges" "us_east_ip_ranges" {
+  regions = ["us-east-1","us-east-1"]
+  services = [ "ec2" ]
+}
+
+resource "aws_security_group" "scgdatasource" {
+  name = "security-terraform"
+  
+  ingress {
+    from_port = "443"
+    to_port = "443"
+    protocol = "tcp"
+    cidr_blocks = data.aws_ip_ranges.us_east_ip_ranges.cidr_blocks
+  }
+
+  tags = {
+    CreateDate = data.aws_ip_ranges.us_east_ip_ranges.create_date
+    SyncToken = data.aws_ip_ranges.us_east_ip_ranges.sync_token
+  }
+}
